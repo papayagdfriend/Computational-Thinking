d8e4b2ad6eaf22e18f860b606f3885aaafaca7a5
papayagdfriend <71421686+papayagdfriend@users.noreply.github.com>
2020-12-17 12:16:01 +0800

    Add files via upload

---
+++ start.py
@@ -0,0 +1,12 @@
+import turtle
+shelly = turtle.Turtle()
+shelly.shape("turtle")
+shelly.position()
+shelly.color("green","blue")
+shelly.pencolor("yellow")
+shelly.turtlesize(10,10,2)
+shelly.resizemode("255")
+shelly.turtlesize(5,5,3)
+for i in range(5):
+    shelly.forward(100)
+    shelly.right(144)
